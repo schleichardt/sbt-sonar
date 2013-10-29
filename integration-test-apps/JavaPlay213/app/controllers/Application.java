452034cd3a27d04d92267e8f688fd417327476cb
Michael Schleichardt <michael@schleichardt.net>
2013-10-29 10:25:27 +0100

    Add bad code to Play example.

--- integration-test-apps/JavaPlay213/app/controllers/Application.java
+++ integration-test-apps/JavaPlay213/app/controllers/Application.java
@@ -8,6 +8,9 @@ import views.html.*;
 public class Application extends Controller {
   
     public static Result index() {
+        try {
+        System.out.println("use logger!!!");
+        } catch(Throwable e){}
         return ok(index.render("Your new application is ready."));
     }
   
