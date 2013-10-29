4089149a2956018377fb63872b75f4556f05c2f5
Michael Schleichardt <michael@schleichardt.net>
2013-10-29 11:36:18 +0100

    Add Xunit test results to sonar analysis.

--- src/main/scala/SbtSonarPlugin.scala
+++ src/main/scala/SbtSonarPlugin.scala
@@ -10,18 +10,46 @@ object SbtSonarPlugin extends Plugin {
 
   val sonarProperties = SettingKey[Map[String, String]]("sonar-properties", "The used properties to configure sonar, see http://docs.codehaus.org/display/SONAR/Analysis+Parameters.")
 
-  override lazy val settings = Seq(generateSonarPropertiesFile <<= (sonarProperties, target) map { (p, targetFolder) =>
-    val propertiesAsString = p.toSeq.map { case (k, v) => "%s=%s".format(k, v) }.mkString("\n")
-    val propertiesFile = targetFolder / "sonar-project.properties"
+  private def filePathsToString(files: Seq[File]) = files.filter(_.exists).map(_.getAbsolutePath).toSet.mkString(",")
+
+  override lazy val settings = Seq(generateSonarPropertiesFile <<= (sonarProperties, target) map { (p, targetDir) =>
+    val resultingMap = p ++ hackForWrongSurefireTestResultNames(targetDir, p)
+    val propertiesAsString = resultingMap.toSeq.map { case (k, v) => "%s=%s".format(k, v) }.mkString("\n")
+    val propertiesFile = targetDir / "sonar-project.properties"
     IO.write(propertiesFile, propertiesAsString)
-  }, sonarProperties <<= (version, organization, name, unmanagedSourceDirectories in Compile) { (v, org, n, sourceDirs) =>
+  }, sonarProperties <<= (version, organization, name, unmanagedSourceDirectories in Compile, unmanagedSourceDirectories in Test, classDirectory in Test, target) { (v, org, n, sourceDirs, testDirs, classDir, targetDir) =>
     Map(
       "sonar.host.url" -> "http://localhost:9000",
       "sonar.projectKey" -> "%s:%s".format(org, n),
       "sonar.projectName" -> n,
       "sonar.projectVersion" -> v,
-      "sonar.sources" -> sourceDirs.filter(_.exists).map(_.getAbsolutePath).toSet.mkString(",")
+      "sonar.dynamicAnalysis" -> "reuseReports",
+      "sonar.tests" -> filePathsToString(testDirs),
+      "sonar.binaries" -> filePathsToString(Seq(classDir)),
+      "sonar.sourceEncoding" -> "UTF-8",
+      "sonar.sources" -> filePathsToString(sourceDirs)
     )
   }
   )
+
+  /**
+   * Play hack, since surefire only accepts tests if they start with TEST-
+   */
+  private def hackForWrongSurefireTestResultNames(targetDir: File, p: Map[String, String]): Map[String, String] = {
+    val guessedTestReportsFolder = targetDir / "test-reports"
+    if (guessedTestReportsFolder.exists && !p.contains("sonar.surefire.reportsPath")) {
+      val surefireTestReportsFolder = targetDir / "test-reports-sonar-copy"
+      IO.copyDirectory(guessedTestReportsFolder, surefireTestReportsFolder, true)
+      IO.listFiles(surefireTestReportsFolder).filter(_.isFile) foreach {
+        testReportFile =>
+          val Prefix = "TEST-"
+          if (!testReportFile.getName.startsWith(Prefix)) {
+            testReportFile.renameTo(new File(testReportFile.getParentFile, Prefix + testReportFile.getName))
+          }
+      }
+      Map("sonar.surefire.reportsPath" -> filePathsToString(Seq(surefireTestReportsFolder)))
+    } else {
+      Map()
+    }
+  }
 }
